4c6d11491128aec5da9ad4ef2336a458dec44f7a
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-07-13 23:05:44 +0600

    added google login and mongodb

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,4 +13,11 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+      baseURL: process.env.BETTER_AUTH_URL, 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
 });
