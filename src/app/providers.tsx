ffd1a2104b9a298f5470143655689c424b29789e
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-07-13 23:22:54 +0600

    fix: remove HeroUIProvider from providers wrapper per HeroUI v3 specification

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { HeroUIProvider } from "@heroui/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <HeroUIProvider className="flex flex-col min-h-screen">
+    <>
       {children}
-    </HeroUIProvider>
+    </>
   );
 }
+
